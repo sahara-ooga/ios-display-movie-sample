49453857b5bd0b7098770933e04e00fe6e2838cc
Yu Ogasawara <26447279+sahara-ooga@users.noreply.github.com>
2022-03-24 18:21:17 +0900

    initial state

---
+++ ios-display-movie-sampleUITests/ios_display_movie_sampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ios_display_movie_sampleUITestsLaunchTests.swift
+//  ios-display-movie-sampleUITests
+//
+//  Created by Yu Ogasawara on 2022/03/24.
+//
+
+import XCTest
+
+class ios_display_movie_sampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
